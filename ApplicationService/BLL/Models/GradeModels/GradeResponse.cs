b9f2938834a13efe55a8888b493b63db71505003
Mirlan01010 <133868984+Mirlan01010@users.noreply.github.com>
2024-04-20 07:01:23 +0600

    miraaaaa

---
+++ ApplicationService/BLL/Models/GradeModels/GradeResponse.cs
@@ -0,0 +1,15 @@
+﻿using ApplicationService.DAL.Contexts;
+using ApplicationService.DAL.Entities;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ApplicationService.BLL.Models.GradeModels
+{
+    public class GradeResponse
+    {
+        public int Id { get; set; }
+        public virtual Exam Exam { get; set; }
+        public virtual ApplicationUser ApplicationUserStud { get; set; }
+        public virtual ApplicationUser ApplicationUserEmp { get; set; }
+        public double StudGrade { get; set; }
+    }
+}
